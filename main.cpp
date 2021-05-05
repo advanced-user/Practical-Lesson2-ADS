d8024eedae321f61c6a2fe85a385932b651e4a2e
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-05 15:16:39 +0300

    Second problem.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,28 @@ int F(int n)
 {
     if(n == 0)
         return 3;
+    else if(n > 0)
+        return 4 * F(n - 1) + 2 * F(n / 2) + 7;
+}
 
-    return 4 * F(n - 1) + 2 * F(n / 2) + 7;
+int Prod(int a, int b)
+{
+    if(b == 0)
+        return 0;
+
+    if(b < 0)
+    {
+        b = -b;
+        a = -a;
+    }
+
+    return a + Prod(a, b-1);
 }
 
 
 int main() {
-    cout << F(3);
-
+    cout << F(3) << endl;
+    cout << Prod(-26, 25) << endl;
 
     return 0;
 }
